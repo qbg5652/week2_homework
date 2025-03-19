dff24255f88de9dd16ba504dc0e0199869df941c
qbg5652 <qbg5652@naver.com>
2025-03-19 16:55:34 +0900

    20250319_깃허브 처음 생성

---
+++ bin/week2_homework.dart
@@ -0,0 +1,5 @@
+import 'package:week2_homework/week2_homework.dart' as week2_homework;
+
+void main(List<String> arguments) {
+  print('Hello world: ${week2_homework.calculate()}!');
+}
---
+++ test/week2_homework_test.dart
@@ -0,0 +1,8 @@
+import 'package:week2_homework/week2_homework.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
