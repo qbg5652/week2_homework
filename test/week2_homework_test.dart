7914ac094bced60e77ed5cdad02a98d9792bdc45
qbg5652 <qbg5652@naver.com>
2025-03-20 11:36:14 +0900

    test 폴더 내 파일 수정

--- test/week2_homework_test.dart
+++ test/week2_homework_test.dart
@@ -1,8 +1,4 @@
 import 'package:week2_homework/week2_homework.dart';
 import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+void main() {}
